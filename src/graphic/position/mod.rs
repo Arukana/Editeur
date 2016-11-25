1d303e59e6de6b897486adf06dcff5c3df6fbf08
Jeremy PEPIN <jpepin@e3r10p13.42.fr>
2016-11-25 02:44:44 +0100

    Remove unused postures

--- src/graphic/position/mod.rs
+++ src/graphic/position/mod.rs
@@ -5,11 +5,8 @@ pub use self::err::{PostureError, Result};
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 pub enum Posture {
     Bust,
-    Lotus,
     Lying,
     Seiza,
-    LotusHandsOnFloor,
-    Joke,
     None,
 }
 
@@ -17,11 +14,8 @@ impl Posture {
     pub fn new(content: &str) -> Result<Self> {
         match content {
             "Bust" => Ok(Posture::Bust),
-            "Lotus" => Ok(Posture::Lotus),
             "Lying" => Ok(Posture::Lying),
             "Seiza" => Ok(Posture::Seiza),
-            "LotusHandsOnFloor" => Ok(Posture::LotusHandsOnFloor),
-            "Joke" => Ok(Posture::Joke),
             "None" => Ok(Posture::None),
             _ => Err(PostureError::UnknownPosture),
         }
