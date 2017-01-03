f8c89c375b57357322b667469717b242c8b8e149
adjivas <adjivas@users.noreply.github.com>
2017-01-03 19:00:49 +0100

    impl trait From for Tuple

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,6 +1,21 @@
+use super::sprite::texel::part::Part;
+use super::emotion::Emotion;
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Default)]
 pub struct Tuple {
-    pub part: super::sprite::texel::part::Part,
-    pub emotion: super::emotion::Emotion,
+    pub part: Part,
+    pub emotion: Emotion,
+}
+
+impl From<(Part, Emotion)> for Tuple {
+
+    /// The constructor `from` returns a tuple of Part and Emotion
+    /// according to the argumentation.
+    fn from((part, emotion): (Part, Emotion)) -> Self {
+        Tuple {
+            part: part,
+            emotion: emotion,
+        }
+    }
 }
